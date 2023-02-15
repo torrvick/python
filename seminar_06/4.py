e7827659088d1535b78d88fa4c479cedad2bb1c2
Vit <torrvick@gmail.com>
2023-02-15 21:27:19 +0300

    seminar_06 Done

--- seminar_06/4.py
+++ seminar_06/4.py
@@ -6,11 +6,18 @@
 # То есть если массив три на четыре, то надо выполнить не более 6 итераций. 
 # И далее в конце опять вывести на экран как таблицу.
 from random import randint
+from random import choice
 
-sp_size = list(map(int,input("Введите размер массива через пробел (строки столбцы): ").split()))
-sp_rows = sp_size[0]
-sp_cols = sp_size[1]
-sp = [[randint(0,10) for _ in range(sp_cols)] for _ in range(sp_rows)]
+while True:
+        sp_size = list(map(int,input("Введите размер массива через пробел (строки столбцы): ").split()))
+        sp_rows = sp_size[0]
+        sp_cols = sp_size[1]
+        if (sp_rows*sp_cols)%2 == 0:
+            break
+        else:
+            print("Количество элементов в массиве должно быть четным. Попробуйте еще раз")
+        
+sp = [[randint(0,99) for _ in range(sp_cols)] for _ in range(sp_rows)]
 # for el in sp:
 #     print(" ".join(list(map(str,el))))
 def print_array(array):
@@ -18,29 +25,29 @@ def print_array(array):
         for el in row:
             print("%3d" %(el), end = " ")
         print()
-        
+
+print()       
 print("Исходный массив:")
 print_array(sp)
 print()
 
-def shake_array(array):
-    array_temp = []
-    for row in array:
-        for el in row:
-            array_temp.append(str(el))
+def gen_coord_table(array):
+    coord_table = []
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            coord_table.append([i,j])
+    return coord_table
 
-    print(array_temp)
-    count_cicles = 0
-    count_mix = 0
-    for i in range(len(array_temp)):
-        el_ind = randint(0,len(array_temp)-1)
-        if array_temp[el_ind].find("_") == -1:
-            el_ind2 = randint(0,len(array_temp)-1)
-            if array_temp[el_ind2].find("_") == -1:
-                array_temp[el_ind],array_temp[el_ind2] = "_" + array_temp[el_ind2], "_" + array_temp[el_ind]
-                count_mix += 1
-        count_cicles += 1
-    print(array_temp)
-    print(count_cicles)
-    print(count_mix)
-shake_array(sp)
+def shake_array(array):
+    coords = gen_coord_table(array)
+    while coords:
+        i, j = choice(coords)
+        coords.remove([i,j])
+        i2, j2 = choice(coords)
+        coords.remove([i2, j2])
+        array[i][j], array[i2][j2] = array[i2][j2], array[i][j]
+    return(array)
+
+print("Перемешанный массив: ")
+print_array(shake_array(sp))
+print()
